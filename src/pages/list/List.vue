1649371b04c23ac5c9c7f2a097e57a02938585b9
rachel0201 <yaorachel99@gmail.com>
2021-12-31 09:46:51 +1100

    init the project with webpack

---
+++ src/pages/list/List.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="list">list</div>
+    <router-link to="/"> back</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "List"
+};
+</script>
+
+<style>
+.list {
+  font-size: 50px;
+  color: red;
+}
+</style>
