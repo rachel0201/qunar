6d353719143646b17281b7f642c61ed671276d08
rachel0201 <yaorachel99@gmail.com>
2021-12-31 16:26:26 +1100

    swiper icon list

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,70 +1,113 @@
 <template>
   <div class="icons">
-    <div class="icon top">
-      <i class="iconfont">&#xe8e9;</i>
-      <p class="des">Hotel</p>
-    </div>
-    <div class="icon top">
-      <i class="iconfont">&#xf841;</i>
-      <p class="des">Flights</p>
-    </div>
-    <div class="icon top">
-      <i class="iconfont">&#xe615;</i>
-      <p class="des">Holidays</p>
-    </div>
-    <div class="icon top">
-      <i class="iconfont">&#xe64d;</i>
-      <p class="des">Landmark</p>
-    </div>
-
-    <div class="icon below">
-      <i class="iconfont">&#xe61b;</i>
-      <p class="des">Cars</p>
-    </div>
-    <div class="icon below">
-      <i class="iconfont">&#xe604;</i>
-      <p class="des">Day trip</p>
-    </div>
-    <div class="icon below">
-      <i class="iconfont">&#xe623;</i>
-      <p class="des">Experience</p>
-    </div>
-    <div class="icon below">
-      <i class="iconfont">&#xe655;</i>
-      <p class="des">Plans</p>
-    </div>
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="(page, index) of pages" :key="index">
+        <div class="icon" v-for="item of page" :key="item.id">
+          <i class="iconfont">{{ item.icon }}</i>
+          <p class="des">{{ item.desc }}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-  name: "HomeIcons"
+  name: "HomeIcons",
+  data() {
+    return {
+      swiperOption: {
+        loop: true
+      },
+      iconList: [
+        {
+          id: 1,
+          icon: "\ue8e9",
+          desc: "Hotel"
+        },
+        {
+          id: 2,
+          icon: "\uf841",
+          desc: "Flights"
+        },
+        {
+          id: 3,
+          icon: "\ue615",
+          desc: "Holidays"
+        },
+        {
+          id: 4,
+          icon: "\ue64d",
+          desc: "Landmark"
+        },
+        {
+          id: 5,
+          icon: "\ue61b",
+          desc: "Cars"
+        },
+        {
+          id: 6,
+          icon: "\ue604",
+          desc: "Day trip"
+        },
+        {
+          id: 7,
+          icon: "\ue623",
+          desc: "Experience"
+        },
+        {
+          id: 8,
+          icon: "\ue655",
+          desc: "Plans"
+        },
+        {
+          id: 9,
+          icon: "\ue740",
+          desc: "Credict"
+        },
+        {
+          id: 10,
+          icon: "\ue611",
+          desc: "Pay Later"
+        }
+      ]
+    };
+  },
+  computed: {
+    pages() {
+      const pages = [];
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8);
+        if (!pages[page]) {
+          pages[page] = [];
+        }
+        pages[page].push(item);
+      });
+      return pages;
+    }
+  }
 };
 </script>
 
 <style lang="stylus" scoped>
-.icons
-  overflow: hidden
+.icons >>> .swiper-container
   height:0
   padding-bottom: 50%
-  .top
-    margin-top: 0.22rem
-  .below
-    margin-top: -0.44rem
-  .icon
-    position:relative
-    float:left
-    width: 25%
-    padding-bottom: 25%
-    height:0
-    .iconfont
-      font-size:1rem
-      top:0
-      left 0
-      right: 0
-      bottom: .44rem
-      padding-left: 0.44rem
-    .des
-      text-align: center
-      color:#333
+.icon
+  position:relative
+  float:left
+  width: 25%
+  padding-bottom: 25%
+  height:0
+  margin-top: 0.22rem
+  margin-bottom: -0.66rem
+  .iconfont
+    font-size:1rem
+    top:0
+    left 0
+    right: 0
+    bottom: .44rem
+    padding-left: 0.44rem
+  .des
+    text-align: center
 </style>
